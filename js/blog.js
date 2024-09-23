bbef801065ede5717b5e776b579d06e8354b4d3a
Sadia0920 <sadiaafrin0920@gmail.com>
2024-09-24 02:20:13 +0600

    added a noakhali donate button function

--- js/blog.js
+++ js/blog.js
@@ -3,6 +3,20 @@
 document.getElementById("btn-blog").addEventListener('click',function(){
     window.location.href='./home.html';
 })
+document.getElementById("btn-donate-noakhali").addEventListener('click',function(){
+    const donateInputNoakhali = getInputElementById('donate-input-noakhali');
+    if(isNaN(donateInputNoakhali)){
+        alert("Failed to add money")
+        return;
+    }
+    
+    const donateAmountNoakhali = getTextElementById('donate-amount-noakhali'); 
+
+    const totalDonateNoakhali = donateAmountNoakhali + donateInputNoakhali;
+    document.getElementById("donate-amount-noakhali").innerText=(totalDonateNoakhali)
+
+})
+
 
 
 
--- js/functionality.js
+++ js/functionality.js
@@ -2,5 +2,14 @@ function showSectionById(id){
     document.getElementById('donation-container').classList.add('hidden');
     document.getElementById('history-container').classList.add('hidden');
     document.getElementById(id).classList.remove('hidden');
-    
+}
+function getInputElementById(id){
+    const inputValue = parseFloat(document.getElementById(id).value);
+    // console.log(inputValue)
+    return inputValue;
+}
+function getTextElementById(id){
+    const inputText = parseFloat(document.getElementById(id).innerText);
+    // console.log(inputText)
+    return inputText;
 }
